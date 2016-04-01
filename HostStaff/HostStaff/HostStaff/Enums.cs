d702697578467419f117348894b1d29469af018e
TeamA2330-001 <w0525326@amarillocollege.com>
2016-03-31 21:21:56 -0500

    add host class
    Start of Project! host class is included in this part of the project,

---
+++ HostStaff/HostStaff/HostStaff/Enums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostStaff
+
+{
+    enum TableStatus{
+        Open = 0,
+        Occupied = 1,
+        Dirty = 2
+    }
+
+    enum Roles
+    {
+        Manager = 0,
+        WaitStaff = 1,
+        Cook = 2,
+        BusBoy = 3,
+        Host = 4
+    }
+}
---
+++ HostStaff/HostStaff/HostStaff/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostStaff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FoodTable table = new FoodTable();
+            table.AssignTable(TableStatus.Open);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
