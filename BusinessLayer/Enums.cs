7e1b39abda8fdc0b25a0beaf4d4f4e6a9dd3500d
Brent Autry <brentautry@bitdraft.com>
2016-04-05 20:44:31 -0500

    New solution structure

---
+++ BusinessLayer/Enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public enum TableStatus
+    {
+        Open = 0,
+        Occupied = 1,
+        Dirty = 2
+    }
+
+    public enum Roles
+    {
+        Manager = 0,
+        WaitStaff = 1,
+        Cook = 2,
+        BusBoy = 3,
+        Host = 4
+    }
+}
+
---
+++ Restaurant/FoodTable.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    class FoodTable
+    {
+        //Primary Key in Database
+        public int FoodTableId { get; set; }
+        //Table Status (Enum)
+        public TableStatus TableStatus { get; set; }
+
+        /// <summary>
+        /// Set Table Status
+        /// </summary>
+        /// <param name="status"></param>
+        public void AssignTable(TableStatus status)
+        {
+            TableStatus = status;
+        }
+
+        //Get table status
+        public TableStatus GetTableStatus()
+        {
+            if (TableStatus == TableStatus.Open)
+            {
+                Console.WriteLine("Table is available");
+            }
+            else if (TableStatus == TableStatus.Occupied)
+            {
+                Console.WriteLine("Table is occupied");
+            }
+            else if (TableStatus == TableStatus.Dirty)
+            {
+                Console.WriteLine("Table is dirty");
+            }
+            return TableStatus;
+        }
+    }
+}
