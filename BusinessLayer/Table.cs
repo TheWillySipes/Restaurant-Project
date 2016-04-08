241fd6ecf1400c892cf4b512195da19d68a57eb3
jmgilbert06 <jmgilbert06@gmail.com>
2016-04-07 21:14:24 -0500

    added business layer classes for ticket handling

---
+++ BusinessLayer/Table.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    class Table
+    {
+        public List<Ticket> Tickets { get; set; }
+
+        public Table()
+        {
+            Tickets = new List<Ticket>();
+        }
+
+        public void AddTicket(Ticket ticket)
+        {
+            Tickets.Add(ticket);
+        }
+    }
+}
---
+++ BusinessLayer/Ticket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    class Ticket
+    {
+        public List<MenuItem> MenuItems { get; set; }
+
+        public Ticket()
+        {
+            MenuItems = new List<MenuItem>();
+        }
+
+        public void AddMenuItem(MenuItem item)
+        {
+            MenuItems.Add(item);
+        }
+    }
+}
