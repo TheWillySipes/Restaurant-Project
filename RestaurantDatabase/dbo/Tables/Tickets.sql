f0dd63f1a29b4661342ef25c4f42473e8ee37593
Brent Autry <brentautry@gmail.com>
2016-04-14 19:34:55 -0500

    Added database project
    A database project will help keep track of changes to a database by
    using schema compares. Also will allow individuals to create scripts to
    setup the database on their own sql server database instance.

---
+++ RestaurantDatabase/dbo/Tables/Tickets.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Tickets] (
+    [ID]            INT      IDENTITY (1, 1) NOT NULL,
+    [TableID]       INT      NOT NULL,
+    [TimePlaced]    DATETIME NOT NULL,
+    [TimeCompleted] DATETIME NULL,
+    CONSTRAINT [PK_Tickets] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_Tickets_FoodTables] FOREIGN KEY ([TableID]) REFERENCES [dbo].[FoodTables] ([ID])
+);
+
---
+++ RestaurantDatabase/dbo/Tables/TicketsMenuItems.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[TicketsMenuItems] (
+    [TicketID]       INT             NOT NULL,
+    [MenuItemID]     INT             NOT NULL,
+    [PricePaid]      DECIMAL (18, 2) NOT NULL,
+    [Returned]       BIT             CONSTRAINT [DF_TicketsMenuItems_Returned] DEFAULT ((0)) NOT NULL,
+    [ReturnReason]   NVARCHAR (250)  NULL,
+    [ManagerComped]  BIT             CONSTRAINT [DF_TicketsMenuItems_ManagerComped] DEFAULT ((0)) NOT NULL,
+    [CompedByUserID] INT             NULL,
+    [ID]             INT             IDENTITY (1, 1) NOT NULL,
+    CONSTRAINT [PK_TicketsMenuItems] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_TicketsMenuItems_MenuItems] FOREIGN KEY ([MenuItemID]) REFERENCES [dbo].[MenuItems] ([ID]),
+    CONSTRAINT [FK_TicketsMenuItems_Tickets] FOREIGN KEY ([TicketID]) REFERENCES [dbo].[Tickets] ([ID]),
+    CONSTRAINT [FK_TicketsMenuItems_Users] FOREIGN KEY ([CompedByUserID]) REFERENCES [dbo].[Users] ([ID])
+);
+
