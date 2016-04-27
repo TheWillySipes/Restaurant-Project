aab811cad1e26a2e048f79c066931a094eee6736
Brent Autry <brentautry@gmail.com>
2016-04-26 20:27:33 -0500

    Implemented initial functionality in data layer and business layer
    Added DataAccessObject in data layer, implemented Employee View Model in
    Business Layer, added RestaurantTests console application for testing
    functionality of application.

---
+++ DataLayer/DataAccessObject.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class DataAccessObject
+    {
+        //Connection string stored statically (bad practice...)
+        private readonly string CONNECTION_STRING = "metadata=res://*/RestaurantEntities.csdl|res://*/RestaurantEntities.ssdl|res://*/RestaurantEntities.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=cis1.actx.edu;initial catalog=RestaurantApplication;user id=db2;password=db20;MultipleActiveResultSets=True;App=EntityFramework&quot;";
+        
+        //Singleton
+        private static DataAccessObject instance;
+        private DataAccessObject() { }
+        public static DataAccessObject Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DataAccessObject();
+                }
+                return instance;
+            }
+        }
+        //End of singleton
+
+        public List<User> GetEmployees()
+        {
+            using(RestaurantApplicationEntities context = new RestaurantApplicationEntities())
+            {
+                return context.Users.ToList();
+            }
+        }
+
+        public bool AuthenticateEmployee(int employeeId, string password)
+        {
+            using(RestaurantApplicationEntities context = new RestaurantApplicationEntities())
+            {
+                var user = context.Users.Where(e => e.ID == employeeId && e.Password == e.Password);
+                if (user == null)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ RestaurantTests/Program.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestBusinessLayer();
+            Console.ReadKey();
+        }
+
+        static void TestBusinessLayer()
+        {
+            var test = EmployeeVM.GetEmployees();
+            if (test.Count > 0)
+            {
+                foreach (var e in test)
+                {
+                    Console.WriteLine("ID: " + e.ID);
+                    Console.WriteLine("UserName: " + e.UserName);
+                    Console.WriteLine("Firstname: " + e.FirstName);
+                    Console.WriteLine("LastName: " + e.LastName);
+                }
+            }
+        }
+    }
+}
