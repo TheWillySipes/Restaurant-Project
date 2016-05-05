d09f3d4b0c26ac9fa6228c3c093e5b1e8ad4f811
jmgilbert06 <jmgilbert06@gmail.com>
2016-05-04 19:48:23 -0500

    update to ticket
    update to tickets

--- Restaurant/CookStaffForm.Designer.cs
+++ Restaurant/CookStaffForm.Designer.cs
@@ -29,34 +29,25 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.btnClockIn = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.completeButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(94, 48);
+            this.label1.Location = new System.Drawing.Point(30, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(156, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Cook Staff Form";
             // 
-            // btnClockIn
-            // 
-            this.btnClockIn.Location = new System.Drawing.Point(13, 296);
-            this.btnClockIn.Name = "btnClockIn";
-            this.btnClockIn.Size = new System.Drawing.Size(75, 23);
-            this.btnClockIn.TabIndex = 1;
-            this.btnClockIn.Text = "Clock In";
-            this.btnClockIn.UseVisualStyleBackColor = true;
-            this.btnClockIn.Click += new System.EventHandler(this.btnClockIn_Click);
-            // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(143, 296);
+            this.btnExit.Location = new System.Drawing.Point(130, 296);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
             this.btnExit.TabIndex = 1;
@@ -64,24 +55,42 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
-            // button3
+            // listBox1
             // 
-            this.button3.Location = new System.Drawing.Point(262, 296);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "Clock Out";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 91);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(193, 186);
+            this.listBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(46, 64);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // completeButton
+            // 
+            this.completeButton.Location = new System.Drawing.Point(12, 296);
+            this.completeButton.Name = "completeButton";
+            this.completeButton.Size = new System.Drawing.Size(75, 23);
+            this.completeButton.TabIndex = 5;
+            this.completeButton.Text = "Done";
+            this.completeButton.UseVisualStyleBackColor = true;
+            this.completeButton.Click += new System.EventHandler(this.completeButton_Click);
             // 
             // CookStaffForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(349, 331);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(228, 331);
+            this.Controls.Add(this.completeButton);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.btnClockIn);
             this.Controls.Add(this.label1);
             this.Name = "CookStaffForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -94,8 +103,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnClockIn;
         private System.Windows.Forms.Button btnExit;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button completeButton;
     }
 }
