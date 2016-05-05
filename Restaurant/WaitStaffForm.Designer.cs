f9b16872f256f802234f714df232881efab3bff8
cbargas <cbargas@actx.edu>
2016-05-05 18:34:55 -0500

    Modifications
    modified wait staff form and class

--- Restaurant/WaitStaffForm.Designer.cs
+++ Restaurant/WaitStaffForm.Designer.cs
@@ -33,17 +33,14 @@
             this.btnCheckOrder = new System.Windows.Forms.Button();
             this.btnCashOut = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.btnClockIn = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
-            this.btnClockOut = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnEnterOrder
             // 
-            this.btnEnterOrder.Enabled = false;
-            this.btnEnterOrder.Location = new System.Drawing.Point(101, 97);
+            this.btnEnterOrder.Location = new System.Drawing.Point(130, 96);
             this.btnEnterOrder.Name = "btnEnterOrder";
-            this.btnEnterOrder.Size = new System.Drawing.Size(75, 36);
+            this.btnEnterOrder.Size = new System.Drawing.Size(105, 67);
             this.btnEnterOrder.TabIndex = 0;
             this.btnEnterOrder.Text = "Enter Order";
             this.btnEnterOrder.UseVisualStyleBackColor = true;
@@ -51,10 +48,9 @@
             // 
             // btnFloorStatus
             // 
-            this.btnFloorStatus.Enabled = false;
-            this.btnFloorStatus.Location = new System.Drawing.Point(20, 139);
+            this.btnFloorStatus.Location = new System.Drawing.Point(12, 140);
             this.btnFloorStatus.Name = "btnFloorStatus";
-            this.btnFloorStatus.Size = new System.Drawing.Size(75, 40);
+            this.btnFloorStatus.Size = new System.Drawing.Size(105, 71);
             this.btnFloorStatus.TabIndex = 1;
             this.btnFloorStatus.Text = "Check Floor Status";
             this.btnFloorStatus.UseVisualStyleBackColor = true;
@@ -62,10 +58,9 @@
             // 
             // btnCheckOrder
             // 
-            this.btnCheckOrder.Enabled = false;
-            this.btnCheckOrder.Location = new System.Drawing.Point(101, 139);
+            this.btnCheckOrder.Location = new System.Drawing.Point(130, 169);
             this.btnCheckOrder.Name = "btnCheckOrder";
-            this.btnCheckOrder.Size = new System.Drawing.Size(75, 41);
+            this.btnCheckOrder.Size = new System.Drawing.Size(105, 72);
             this.btnCheckOrder.TabIndex = 2;
             this.btnCheckOrder.Text = "Check Order Status";
             this.btnCheckOrder.UseVisualStyleBackColor = true;
@@ -73,10 +68,9 @@
             // 
             // btnCashOut
             // 
-            this.btnCashOut.Enabled = false;
-            this.btnCashOut.Location = new System.Drawing.Point(182, 139);
+            this.btnCashOut.Location = new System.Drawing.Point(251, 139);
             this.btnCashOut.Name = "btnCashOut";
-            this.btnCashOut.Size = new System.Drawing.Size(75, 41);
+            this.btnCashOut.Size = new System.Drawing.Size(105, 72);
             this.btnCashOut.TabIndex = 3;
             this.btnCashOut.Text = "Cash Out Customer";
             this.btnCashOut.UseVisualStyleBackColor = true;
@@ -86,51 +80,29 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(29, 31);
+            this.label1.Location = new System.Drawing.Point(66, 31);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(228, 32);
             this.label1.TabIndex = 4;
             this.label1.Text = "Wait Staff Screen";
             // 
-            // btnClockIn
-            // 
-            this.btnClockIn.Location = new System.Drawing.Point(12, 226);
-            this.btnClockIn.Name = "btnClockIn";
-            this.btnClockIn.Size = new System.Drawing.Size(75, 23);
-            this.btnClockIn.TabIndex = 5;
-            this.btnClockIn.Text = "Clock In";
-            this.btnClockIn.UseVisualStyleBackColor = true;
-            this.btnClockIn.Click += new System.EventHandler(this.btnClockIn_Click);
-            // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(101, 226);
+            this.btnExit.Location = new System.Drawing.Point(130, 292);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.Size = new System.Drawing.Size(105, 54);
             this.btnExit.TabIndex = 6;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
-            // 
-            // btnClockOut
-            // 
-            this.btnClockOut.Location = new System.Drawing.Point(182, 226);
-            this.btnClockOut.Name = "btnClockOut";
-            this.btnClockOut.Size = new System.Drawing.Size(75, 23);
-            this.btnClockOut.TabIndex = 7;
-            this.btnClockOut.Text = "Clock Out";
-            this.btnClockOut.UseVisualStyleBackColor = true;
-            this.btnClockOut.Click += new System.EventHandler(this.btnClockOut_Click);
             // 
             // WaitStaffForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.btnClockOut);
+            this.ClientSize = new System.Drawing.Size(368, 358);
             this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.btnClockIn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCashOut);
             this.Controls.Add(this.btnCheckOrder);
@@ -152,8 +124,6 @@
         private System.Windows.Forms.Button btnCheckOrder;
         private System.Windows.Forms.Button btnCashOut;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnClockIn;
         private System.Windows.Forms.Button btnExit;
-        private System.Windows.Forms.Button btnClockOut;
     }
 }
